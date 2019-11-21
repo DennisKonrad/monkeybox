281cab2f58da00d2f55d895531754560a7ec11a3
Dennis Konrad <d.konrad@intranet.ewerk.com>
2019-11-21 17:12:44 +0100

    Added OvS based kvm

---
+++ centos7-kvm-ovs/scripts/00base.sh
@@ -0,0 +1,23 @@
+set -eux
+
+yum -y update
+
+cat > /etc/motd << EOF
+
+   __?.o/  CentOS 7 KVM MonkeyBox
+  (  )#    Built from https://github.com/rhtyd/monkeybox.git
+ (___(_)   Happy CloudStack hacking!
+
+EOF
+
+# Essentials
+yum install -y tmux vim htop wget jq
+
+# Fix hostname to get from dhcp, otherwise use localhost
+hostnamectl set-hostname localhost --static
+
+# Setup public key access
+mkdir -pm 700 /root/.ssh
+# TODO ...
+curl https://api.github.com/users/rhtyd/keys | jq -r '.[].key' > /root/.ssh/authorized_keys
+chmod -R go-rwsx /root/.ssh
