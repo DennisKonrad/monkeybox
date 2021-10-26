74db903f1259e6540ccc8b7971abed50e6017c32
Dennis Konrad <Dennis Konrad@EW-NB-0142.intranet.ewerk.com>
2021-10-26 14:45:13 +0200

    Delete old alma directories

--- alma-kvm-ovs/scripts/01grub.sh
+++
@@ -1,7 +0,0 @@
-set -eux
-
-# Get eth0 eth1 as default nics
-sed -i 's/quiet.*/quiet net.ifnames=0"/g' /etc/default/grub
-grub2-mkconfig -o /boot/grub2/grub.cfg
-
-exit 0
--- alma-mgmtnfs/scripts/02sshd.sh
+++
@@ -1,6 +0,0 @@
-set -eux
-
-sed -i 's/^GSS/\#GSS/g' /etc/ssh/sshd_config
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-echo "UseDNS no" >> /etc/ssh/sshd_config
-
